ffb84c74a6d67b48143056ae691a46b4c2389c32
strongcookdas <goodjav141592@gmail.com>
2023-12-04 00:03:06 +0900

    docs : 리드미 추가

--- ch4-todo-read-view/App.js
+++ ch4-todo-read-view/App.js
@@ -2,6 +2,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import ManageView from "./screens/ManageView";
 import MainView from "./screens/MainView";
+import IconButton from "./components/IconButton";
 
 const Stack = createNativeStackNavigator();
 
@@ -9,7 +10,22 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="main" component={MainView} />
+        <Stack.Screen
+          name="main"
+          component={MainView}
+          options={({ navigation }) => ({
+            headerRight: () => (
+              <IconButton
+                icon="add"
+                size={30}
+                color="black"
+                onPress={() => {
+                  navigation.navigate("manage");
+                }}
+              />
+            ),
+          })}
+        />
         <Stack.Screen name="manage" component={ManageView} />
       </Stack.Navigator>
     </NavigationContainer>
