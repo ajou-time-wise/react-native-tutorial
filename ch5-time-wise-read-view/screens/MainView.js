57d0ee415f22872de816339522a5375a2fea3da4
strongcookdas <goodjav141592@gmail.com>
2023-12-04 00:45:49 +0900

    feat : 미니 캘린더 생성

--- ch5-time-wise-read-view/screens/MainView.js
+++ ch5-time-wise-read-view/screens/MainView.js
@@ -1,11 +1,20 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import MiniCalendar from "../components/main/MiniCalendar";
+import { useState } from "react";
 
 function MainView() {
+  const [selectedDate, setSeletedDate] = useState(new Date());
   return (
-    <View>
-      <Text>MainView</Text>
+    <View style={styles.container}>
+      <MiniCalendar setSeletedDate={setSeletedDate} />
     </View>
   );
 }
 
 export default MainView;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
